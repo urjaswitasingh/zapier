d90bc79605ddaca6c9eecdd19e2e35af2c95bba5
unknown <vijaychaurasiya911@gmail.com>
2024-11-14 14:45:57 +0530

    commit

--- src/main/java/com/example/demo/kafka/KafkaProducerService.java
+++ src/main/java/com/example/demo/kafka/KafkaProducerService.java
@@ -4,9 +4,11 @@ import com.example.demo.dto.CreditCardApplicationDTO;
 import com.example.demo.model.CreditCardApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+@Service
 public class KafkaProducerService {
-    private static final String TOPIC = "credit-check-topic";
+    private static final String TOPIC = "credit-check";
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
     public void sendMessage(CreditCardApplication message){
